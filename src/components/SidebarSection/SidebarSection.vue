39a790844e59017125bdfba6bf858cbf3e21f6c2
Alexandr Drobot <drobot.personal@gmail.com>
2024-07-21 18:11:15 +0300

    work in SidebarSection

--- src/components/SidebarSection/SidebarSection.vue
+++ src/components/SidebarSection/SidebarSection.vue
@@ -1,32 +1,56 @@
 <template>
   <div class="SidebarSection">
     <div class="SidebarSection__header">
-      <div class="SidebarSection__header-menu"></div>
+      <div class="SidebarSection__header-menu">
+        <div class="dropdown">
+          <span class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list"
+                 viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>
+          </span>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Выход</a></li>
+          </ul>
+        </div>
+      </div>
       <div class="SidebarSection__header-search">
-        <input class="SidebarSection__header-search-input" type="text" placeholder="Search"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+        </svg>
+        <input class="SidebarSection__header-search-input" type="search" placeholder="Поиск"/>
       </div>
       <div class="SidebarSection__header-profile"></div>
     </div>
     <div class="SidebarSection__chats">
       <div class="SidebarSection__chats-filter">
-        <div class="SidebarSection__chats-filter-item">
+        <div class="SidebarSection__chats-filter-item SidebarSection__chats-filter-item--active">
           Все
         </div>
       </div>
-      <div class="SidebarSection__chats-items">
-        <div class="SidebarSection__chats-item">
-          <img src="" alt="" class="SidebarSection__chats-item-img">
-          <div class="SidebarSection__chats-item-context">
-            <div class="SidebarSection__chats-item-context-title">Saved Messages</div>
-            <div class="SidebarSection__chats-item-context-message">My text example....</div>
+      <div v-if="messages" class="SidebarSection__chats-items">
+        <router-link v-for="message in messages" v-bind:key="message.id" to="/" class="SidebarSection__chats-item">
+          <div class="SidebarSection__chats-item-img">
+            <span>{{ message.user.avatar.letter }}</span>
           </div>
-          <div class="SidebarSection__chats-item-helper">
-            <div class="SidebarSection__chats-item-helper-read-status">-</div>
-            <div class="SidebarSection__chats-item-helper-time">
-              15:04
+          <div class="SidebarSection__chats-item-context">
+            <div class="SidebarSection__chats-item-context-title">
+              <span v-html="message.title"></span>
+            </div>
+            <div class="SidebarSection__chats-item-context-message">
+              <span v-html="message.text"></span>
             </div>
           </div>
-        </div>
+          <div class="SidebarSection__chats-item-helper">
+            <div class="SidebarSection__chats-item-helper-read-status">
+              <SidebarSectionIsReadStatus/>
+            </div>
+            <div class="SidebarSection__chats-item-helper-time">
+              <span v-html="message.time"></span>
+            </div>
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
@@ -35,9 +59,49 @@
 <script lang="ts">
 import {defineComponent} from 'vue';
 import '@/components/SidebarSection/SidebarSection.css';
+import SidebarSectionIsReadStatus from "@/components/SidebarSection/components/SidebarSectionIsReadStatus.vue";
 
 export default defineComponent({
   name: 'SidebarSection',
-  components: {},
+  components: {SidebarSectionIsReadStatus},
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          title: 'Инна',
+          text: 'Добрый день! Что вас интересует...',
+          user: {
+            avatar: {
+              letter: 'И'
+            }
+          },
+          time: '15:04'
+        },
+        {
+          id: 2,
+          title: 'Сергей',
+          text: 'Я хотел бы узнать у вас...',
+          user: {
+            avatar: {
+              letter: 'С'
+            }
+          },
+          time: '11:23'
+        },
+        {
+          id: 3,
+          title: 'Марина',
+          text: 'Ок',
+          user: {
+            avatar: {
+              letter: 'М'
+            }
+          },
+          time: '15:52'
+        }
+      ]
+    };
+  }
 });
 </script>
