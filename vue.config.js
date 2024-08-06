cf95ca88f8ae675abae0d38633f46cfdbf7316ba
Alexandr Drobot <drobot.personal@gmail.com>
2024-08-06 22:28:44 +0300

    update build path

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: process.env.NODE_ENV === 'production'
         ? '/chat/'
-        : '/'
+        : '/',
+    assetsDir: process.env.NODE_ENV === 'production' ? "Front-end/dist" : undefined
 })
